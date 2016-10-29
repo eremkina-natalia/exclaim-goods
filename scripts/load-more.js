f3ec5b52a74bc507d8bb334ab6e6a726d4a46d11
eremkina-natalia <natusik-one@mail.ru>
2016-10-29 22:55:03 +0300

    Add load data on scroll

--- scripts/load-more.js
+++ scripts/load-more.js
@@ -27,8 +27,7 @@ function createItemNode (item) {
 
   return itemNode;
 }
-
-function moreButtonClickHandler () {
+/*function moreButtonClickHandler () {
   loadJSON('data.json', function (data) {
     var nodes = data.goods.map(createItemNode);
     nodes.forEach(function (itemNode) {
@@ -36,5 +35,25 @@ function moreButtonClickHandler () {
     })
   });
 }
+moreButton.addEventListener('click', moreButtonClickHandler);*/
 
-moreButton.addEventListener('click', moreButtonClickHandler);
+var scrollTop = document.body.scrollTop;
+function dataScroll() {
+  var scrollTop = document.body.scrollTop;
+//  var offsetTop = document.body.offsetTop;
+//  var clientHeight = document.body.clientHeight;
+//console.log('offsetTop ' + offsetTop); //0
+//console.log('scrollTop ' + scrollTop); //828
+//console.log('clientHeight ' + clientHeight); //1345
+//console.log('window.pageYOffset ' +window.pageYOffset); //828
+//console.log('scrollHeight ' +document.body.scrollHeight); //1345
+  if (scrollTop > document.body.offsetHeight - 550){
+    loadJSON('data.json', function (data) {
+      var nodes = data.goods.map(createItemNode);
+      nodes.forEach(function (itemNode) {
+        elementsNode.appendChild(itemNode);
+      })
+    });
+  }
+};
+document.addEventListener('scroll', dataScroll);
